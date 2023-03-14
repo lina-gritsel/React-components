4721beb2883aac72139f9d4d2035c4b16bee3c0a
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-03-14 19:12:06 +0300

    refactor: add menu in header

--- react-components/src/components/Header/Header.tsx
+++ react-components/src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
 import Menu from '../Menu'
+import logo from '../../assets/logo.png'
 
 import styles from './Header.module.scss'
 
 const Header: FC = () => {
   return (
     <div className={styles.container}>
+      <img className={styles.logo} src={logo} />
       <Menu />
     </div>
   )
--- react-components/src/components/Menu/Menu.tsx
+++ react-components/src/components/Menu/Menu.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react'
 import { NavLink } from 'react-router-dom'
 import { MENU_ITEM } from './constants'
 
+import styles from './Menu.module.scss'
+
 const Menu: FC = () => {
   return (
-    <div>
+    <div className={styles.menuContent}>
       {MENU_ITEM.map(({ label, to }, index) => (
-        <NavLink key={index} to={to}>
+        <NavLink key={index} to={to} className={styles.menuItem}>
           {label}
         </NavLink>
       ))}
