2572c1c8328b0464b7095ba48c312cd0056fff05
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-03-22 09:27:55 +0300

    refactor: refactor config coverage

--- react-components/vite.config.ts
+++ react-components/vite.config.ts
@@ -4,7 +4,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
@@ -13,6 +12,7 @@ export default defineConfig({
     setupFiles: './src/test/setup.ts',
     coverage: {
       all: true,
+      reporter: ['text'],
     },
   },
 })
