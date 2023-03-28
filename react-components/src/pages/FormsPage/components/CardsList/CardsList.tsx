f04b79b4b02ce7e8e4a0df52394905fdce0c6f0c
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-03-28 16:49:01 +0300

    feat: change cards list in form page

--- react-components/src/pages/FormsPage/components/CardsList/CardsList.tsx
+++ react-components/src/pages/FormsPage/components/CardsList/CardsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { FC } from 'react'
 import { ICardData } from '../../../../pages/types'
 
 import styles from './CardsList.module.scss'
@@ -7,38 +7,34 @@ interface CardsListProps {
   cards?: ICardData[]
 }
 
-class CardsList extends React.Component<CardsListProps> {
-  render() {
-    return (
-      <div className={styles.container}>
-        {this.props.cards?.map(
-          ({ name, birth, category, image, switcher }, index) => (
-            <div key={index} className={styles.card}>
-              <div className={styles.cardHeader}>
-                <img src={image} className={styles.image} />
-                <div className={styles.dataUser}>
-                  <div className={styles.infoUser}>
-                    <span className={styles.subtitle}>Name:</span> {name}
-                  </div>
-                  <div className={styles.infoUser}>
-                    <span className={styles.subtitle}>Birth:</span> {birth}
-                  </div>
-                  <div className={styles.infoUser}>
-                    <span className={styles.subtitle}>Category:</span>
-                    {category}
-                  </div>
-                  <div className={styles.infoUser}>
-                    <span className={styles.subtitle}>Notices:</span>
-                    {switcher}
-                  </div>
-                </div>
+const CardsList: FC<CardsListProps> = ({ cards }) => {
+  return (
+    <div className={styles.container}>
+      {cards?.map(({ name, birth, category, image, switcher }, index) => (
+        <div key={index} className={styles.card}>
+          <div className={styles.cardHeader}>
+            <img src={image} className={styles.image} />
+            <div className={styles.dataUser}>
+              <div className={styles.infoUser}>
+                <span className={styles.subtitle}>Name:</span> {name}
+              </div>
+              <div className={styles.infoUser}>
+                <span className={styles.subtitle}>Birth:</span> {birth}
+              </div>
+              <div className={styles.infoUser}>
+                <span className={styles.subtitle}>Category:</span>
+                {category}
+              </div>
+              <div className={styles.infoUser}>
+                <span className={styles.subtitle}>Notices:</span>
+                {switcher}
               </div>
             </div>
-          )
-        )}
-      </div>
-    )
-  }
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default CardsList
