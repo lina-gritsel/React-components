8fe9c743c69a30b3827577c6c750fb972dcf917f
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-03-28 11:57:03 +0300

    refactor: refactor header

--- react-components/src/components/Header/Header.tsx
+++ react-components/src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import { FC } from 'react'
 
 import logo from '../../assets/logo.png'
-
 import Menu from '../Menu'
 
 import styles from './Header.module.scss'
@@ -10,20 +9,18 @@ interface HeaderProps {
   currentPage?: string
 }
 
-class Header extends React.Component<HeaderProps> {
-  render() {
-    return (
-      <div className={styles.container}>
-        <div className={styles.contentWrapper}>
-          <img className={styles.logo} src={logo} />
-          <div className={styles.currentPageWrapper}>
-            <div className={styles.title}> Current Page:</div>
-            <div className={styles.currentPage}>{this.props.currentPage}</div>
-          </div>
-          <Menu />
+const Header: FC<HeaderProps> = ({ currentPage }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.contentWrapper}>
+        <img className={styles.logo} src={logo} />
+        <div className={styles.currentPageWrapper}>
+          <div className={styles.title}> Current Page:</div>
+          <div className={styles.currentPage}>{currentPage}</div>
         </div>
+        <Menu />
       </div>
-    )
-  }
+    </div>
+  )
 }
 export default Header
