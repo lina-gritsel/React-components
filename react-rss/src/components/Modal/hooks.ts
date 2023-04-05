f8dd478dccaf73d01dc7fba411a8b8b21576346d
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-04-05 13:25:38 +0300

    feat: add locked body

--- react-rss/src/components/Modal/hooks.ts
+++ react-rss/src/components/Modal/hooks.ts
@@ -1,11 +1,24 @@
 import { useState } from 'react'
+import { useLockedBody } from '../../hooks'
 
 export const useModal = () => {
+  const [setLocked] = useLockedBody()
+
   const [visible, setVisible] = useState<boolean>(false)
+
+  const open = () => {
+    setVisible(true)
+    setLocked(true)
+  }
+
+  const close = () => {
+    setVisible(false)
+    setLocked(false)
+  }
 
   return {
     visible,
-    open: () => setVisible(true),
-    close: () => setVisible(false),
+    open,
+    close,
   }
 }
