0937e1f3202615d84326c32cc857de788ea2dca5
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-04-25 14:34:09 +0300

    feat: add coverage for e2e tests

--- react-rss/cypress.config.ts
+++ react-rss/cypress.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'cypress'
+import coverage from '@cypress/code-coverage/task'
 
 export default defineConfig({
+  video: false,
+  env: {
+    codeCoverage: {
+      exclude: 'cypress/**/*.*',
+    },
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      coverage(on, config)
+      return config
     },
     baseUrl: 'http://localhost:3000',
   },
