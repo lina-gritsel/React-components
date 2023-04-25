35cfcab05209573a8740d1783ddc7c02cfc2160b
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-04-25 22:21:47 +0300

    refactor: refactor code

--- react-rss/src/main.tsx
+++ react-rss/src/main.tsx
@@ -1,17 +1,17 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import { Provider } from 'react-redux'
-// import store from './store/store'
-// import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './store/store'
+import App from './App'
 
-// import './scss/styles.scss'
+import './scss/styles.scss'
 
-// const storeApp = store()
+const storeApp = store()
 
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <Provider store={storeApp}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>
-// )
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Provider store={storeApp}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+)
